4c65aafdc56af74982753b75feee941abfc68b50
raya19998333 <rayaalamri90@gmail.com>
2024-12-25 10:41:11 -0800

    solve route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./component/Header";
 import Home from "./component/Home";
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Use BrowserRouter instead of Router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./component/Products";
 import About from "./component/About";
 import Footer from "./component/Footer";
@@ -16,11 +16,6 @@ import { useSelector } from "react-redux";
 import Cart from "./component/Cart";
 import SharePost from "./component/SharePost";
 import Manage from "./component/Admin/Manage";
-import ManageProfile from "./component/Admin/ManageProfile";
-import DisplayPosts from "./component/Posts";
-import AdminPostsTable from "./component/Admin/Feedbacks";
-import ManageProducts from "./component/Admin/ManageProducts";
-import UpdateProduct from "./component/Admin/UpdateProduct";
 import AdminCartDashboard from "./component/Admin/AdminCarts";
 
 function App() {
@@ -28,46 +23,25 @@ function App() {
   return (
     <div>
       <Router>
-        <Row>
-          {email ? (
-            <>
-              <Header />
-            </>
-          ) : null}
-        </Row>
+        <Row>{email && <Header />}</Row>
         <Container>
           <Routes>
-            {" "}
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/allcarts" element={<AdminCartDashboard />} />
-            <Route path="/manage" element={<Manage />}></Route>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/manage" element={<Manage />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/products" element={<Products />} />
             <Route path="/about" element={<About />} />
             <Route path="/registerUser" element={<Register />} />
             <Route path="/Liptint" element={<Liptint />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/order/:orderId" element={<Order />} />
-            <Route path="/SharePost" element={<SharePost />}></Route>
-            <Route path="/posts" element={<AdminPostsTable />}></Route>
-            <Route path="/managep" element={<ManageProducts />}></Route>
-            <Route path="/update/:prod_id" element={<UpdateProduct />}></Route>
+            <Route path="*" element={<div>Page Not Found</div>} />
           </Routes>
         </Container>
       </Router>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Row>
-        {email ? (
-          <>
-            <Footer />
-          </>
-        ) : null}
-      </Row>
+      <Row>{email && <Footer />}</Row>
     </div>
   );
 }
